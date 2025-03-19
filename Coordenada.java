127bb5cd41ed249ba419aaeb13c6a304eca6b436
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-19 10:37:13 -0500

    arreglando nombre clase

--- Coordenada.java
+++ Coordenada.java
@@ -1,16 +1,18 @@
 import java.util.*;
 
-public class Coodenada{
+public class Coordenada{
     private double x;
     private double y;
 
-    public Coodenada(){
+    public Coordenada(){
         this.x = 0.0;
         this.y = 0.0;
     }
 
-    public Coodenada(double x, double y){
+    public Coordenada(double x, double y){
         this.x = x;
         this.y = y;
     }
+
+    public 
 }
