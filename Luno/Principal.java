d202cf269321e291e77db4863ae51ad67c257148
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-19 21:50:33 -0500

    principal

--- Luno/Principal.java
+++ Luno/Principal.java
@@ -4,6 +4,10 @@ import Luno.Rectangulo;
 
 public class Principal{
     public static void main(String[] args){
-        
+        Rectangulo A = new Rectangulo(new Coordenada(1.5, 0.3), new Coordenada(7.6, 2.2));
+        Rectangulo B = new Rectangulo(new Coordenada(4.0, 4.2), new Coordenada(9.4, -2.5));
+
+        System.out.println(A);
+        System.out.println(B);
     }
 }
