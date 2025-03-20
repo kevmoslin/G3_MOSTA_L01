93c4df951480d517370521c0ce0b8ff98c6dabe9
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-20 08:59:07 -0500

    ingreso de coordenadas por teclado

--- Luno/Principal.java
+++ Luno/Principal.java
@@ -1,11 +1,30 @@
 package Luno;
+import java.util.Scanner;
 import Luno.Coordenada;
 import Luno.Rectangulo;
 
 public class Principal{
     public static void main(String[] args){
-        Rectangulo A = new Rectangulo(new Coordenada(1.5, 0.3), new Coordenada(7.6, 2.2));
-        Rectangulo B = new Rectangulo(new Coordenada(4.0, 4.2), new Coordenada(9.4, -2.5));
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("ingrese una esquina del primer recatangulo: ");
+        double x1 = scan.nextDouble();
+        double y1 = scan.nextDouble();
+
+        System.out.println("ingrese la otra esquina del primer recatangulo: ");
+        double x2 = scan.nextDouble();
+        double y2 = scan.nextDouble();
+
+        System.out.println("ingrese una esquina del segundo rectangulo: ");
+        double x3 = scan.nextDouble();
+        double y3 = scan.nextDouble();
+
+        System.out.println("ingrese otra esquina del segundo rectangulo: ");
+        double x4 = scan.nextDouble();
+        double y4 = scan.nextDouble();
+        
+        Rectangulo A = new Rectangulo(new Coordenada(x1, y1), new Coordenada(x2, y2));
+        Rectangulo B = new Rectangulo(new Coordenada(x3, y3), new Coordenada(x4, y4));
 
         System.out.println(A);
         System.out.println(B);
