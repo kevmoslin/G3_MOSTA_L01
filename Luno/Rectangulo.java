3b0eebcc47229f4aa842268d34e8e55171a3778b
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-20 09:25:39 -0500

    arreglando rectangulos

--- Luno/Rectangulo.java
+++ Luno/Rectangulo.java
@@ -7,6 +7,24 @@ public class Rectangulo{
     private Coordenada esquina2;
 
     public Rectangulo(Coordenada c1, Coordenada c2){
+        double minX, minY, maxX, maxY;
+
+        if (c1.getX() < c2.getX()) {
+            minX = c1.getX();
+            maxX = c2.getX();
+        } else{
+            minX = c2.getX();
+            maxX = c1.getX();
+        }
+
+        if (c1.getY() < c2.getY()) {
+            minY = c1.getY();
+            maxX = c2.getY();
+        } else{
+            minY = c2.getY();
+            maxX = c1.getY();
+        }
+        
         this.esquina1 = c1;
         this.esquina2 = c2;
     }
