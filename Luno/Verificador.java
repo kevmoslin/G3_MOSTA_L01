1126b38840662f379509dd42346c0faba1e5b0fa
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-19 22:30:49 -0500

    verificador arreglado y terminado

--- Luno/Verificador.java
+++ Luno/Verificador.java
@@ -8,8 +8,10 @@ public class Verificador {
             return "los rectangulos estan disjuntos";
         }
 
-        if () {
-            
+        if ((A.getEsquina2().getX() == B.getEsquina1().getX() || B.getEsquina2().getX() == A.getEsquina1().getX()) || (A.getEsquina2().getY() == B.getEsquina1().getY() || B.getEsquina1().getY() ==  A.getEsquina2().getY())) {
+            return "los rectangulos se juntan";
         }
+
+        return "los recatangulos estan sobrepuestos";
     }
 }
