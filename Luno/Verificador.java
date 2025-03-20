33e9f6a8907a2c9137fdc893c623d6d8eedbf829
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-19 22:03:15 -0500

    verificador

---
+++ Luno/Verificador.java
@@ -0,0 +1,15 @@
+package Luno;
+import Luno.Coordenada;
+import Luno.Rectangulo;
+
+public class Verificador {
+    static String verificadorRectangulos(Rectangulo A, Rectangulo B){
+        if (A.getEsquina2().getX() < B.getEsquina1().getX() || B.getEsquina2().getX() < A.getEsquina1().getX() || A.getEsquina2().getY() < B.getEsquina1().getY() || B.getEsquina2().getY() < A.getEsquina1().getY()) {
+            return "los rectangulos estan disjuntos";
+        }
+
+        if () {
+            
+        }
+    }
+}
