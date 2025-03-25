d9ac7309f0c04b0fff363f4e86f0ee96cbb7371b
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-24 21:16:16 -0500

    arreglado: ajuste el codigo para que entre en una captura para las diapositicas

--- Luno/Verificador.java
+++ Luno/Verificador.java
@@ -4,11 +4,13 @@ import Luno.Rectangulo;
 
 public class Verificador {
     static String verificadorRectangulos(Rectangulo A, Rectangulo B){
-        if (A.getEsquina2().getX() < B.getEsquina1().getX() || B.getEsquina2().getX() < A.getEsquina1().getX() || A.getEsquina2().getY() < B.getEsquina1().getY() || B.getEsquina2().getY() < A.getEsquina1().getY()) {
+        if (A.getEsquina2().getX() < B.getEsquina1().getX() || B.getEsquina2().getX() < A.getEsquina1().getX() 
+        || A.getEsquina2().getY() < B.getEsquina1().getY() || B.getEsquina2().getY() < A.getEsquina1().getY()) {
             return "los rectangulos estan disjuntos";
         }
 
-        if ((A.getEsquina2().getX() == B.getEsquina1().getX() || B.getEsquina2().getX() == A.getEsquina1().getX()) || (A.getEsquina2().getY() == B.getEsquina1().getY() || B.getEsquina1().getY() ==  A.getEsquina2().getY())) {
+        if ((A.getEsquina2().getX() == B.getEsquina1().getX() || B.getEsquina2().getX() == A.getEsquina1().getX()) 
+        || (A.getEsquina2().getY() == B.getEsquina1().getY() || B.getEsquina1().getY() ==  A.getEsquina2().getY())) {
             return "los rectangulos se juntan";
         }
 
