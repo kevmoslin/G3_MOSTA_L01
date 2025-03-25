a60411d84e33799a8f2ac5f325e59de1c8ba4dc2
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-25 18:07:17 -0500

    atributos container

--- Luno/ContainerRect.java
+++ Luno/ContainerRect.java
@@ -2,5 +2,9 @@ package Luno;
 import java.util.ArrayList;
 
 public class ContainerRect {
-    
+    private Rectangulo[] rectangulos;
+    private double[] distancias;
+    private double[] areas;
+    private int n;
+    private static int numRec = 0; 
 }
