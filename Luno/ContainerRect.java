81ee8244d474d23647ea47e9942f81c72d00366e
kevmoslin <kevinmostacerolinares@gmail.com>
2025-03-25 18:24:56 -0500

    metodos

--- Luno/ContainerRect.java
+++ Luno/ContainerRect.java
@@ -15,6 +15,14 @@ public class ContainerRect {
         this.areas = new double[n];
     }
 
+    private double calcularDistanciaEuclidiana(Rectangulo r){
+        return Math.sqrt(Math.pow(r.getEsquina1().getX() - r.getEsquina2().getX(),2) + Math.pow(r.getEsquina1().getY() - r.getEsquina2().getY(),2)); 
+    }
+
+    private double calcularArea(Rectangulo r){
+        return Math.abs((r.getEsquina2().getX() - r.getEsquina1().getX()) * (r.getEsquina2().getY() - r.getEsquina1().getY()));
+    }
+
     public boolean agregarRectangulo(Rectangulo r){
         if (numRec < n) {
             rectangulos[numRec] = r;
@@ -26,6 +34,7 @@ public class ContainerRect {
             return false;
         }
     }
+
     private void mostrarRectangulos(){
         for(int i = 0; i < numRec; i++){
             System.out.println("rectangulo " + (i + 1) + ": " + rectangulos[i]);
